6d6ffb4011ed209d0f61c0ead9a223d6eca5035a
mrcruz117 <mrcruz1989@gmail.com>
2024-08-17 20:57:21 -0400

    dividing chunks

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from pytubefix.cli import on_progress
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+from pydub import AudioSegment
+import io
+
 
 load_dotenv()
 
@@ -18,15 +21,39 @@ yt = YouTube(yt_vid_link, on_progress_callback=on_progress)
 print(yt.title)
 
 
-audio_streams = yt.streams.filter(only_audio=True, file_extension="mp4")
+audio_streams = yt.streams.filter(only_audio=True)
 
+# print("Audio Streams: ", audio_streams)
 ys = yt.streams.get_audio_only()
+print(ys)
+
 ys.download(mp3=True, output_path="downloads")
 
 file_path = "downloads/" + yt.title + ".mp3"
 
-audio_file = open(file_path, "rb")
+# audio_file = open(file_path, "rb")
+audio_segment = AudioSegment.from_file(file_path, "mp4")
 
-transcription = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
+chunk_length_ms = 1000 * 60  # 1 min
+
+first_1_minute = audio_segment[:chunk_length_ms]
+
+# Convert the AudioSegment to a file-like object
+first_1_minute.export("chunks/clip.mp3", format="mp3")
+
+audio_file = open("chunks/clip.mp3", "rb")
+
+
+transcription = client.audio.transcriptions.create(
+    model="whisper-1", file=audio_file
+)
 
 print(transcription.text)
+
+# for i, chunk in enumerate(chunks):
+#     chunk_io = io.BytesIO()
+#     chunk.export(chunk_io, format="mp3")
+#     chunk_io.seek(0)
+
+#     transcription = client.audio.transcriptions.create(model="whisper-1", file=chunk_io)
+#     print(f"Chunk {i + 1}/{len(chunks)}: {transcription.text}")
