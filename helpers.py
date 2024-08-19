017772492176b51332010b91bb5e826a91f736b5
mrcruz117 <mrcruz1989@gmail.com>
2024-08-19 09:48:23 -0400

    new helper func

--- helpers.py
+++ helpers.py
@@ -1,5 +1,19 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+from pydub import AudioSegment
+import time
+
+load_dotenv()
+
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 
 def dl_yt_audio(link, target_path):
@@ -27,3 +41,43 @@ def dl_yt_audio(link, target_path):
     ys.download(mp3=True, output_path=target_path)
 
     return vid_uuid, len_in_ms, yt.title
+
+
+def transcribe_or_translate_audio(file_path, len_in_ms, vid_uuid):
+    audio_segment = AudioSegment.from_file(file_path, "mp4")
+
+    chunk_length_ms = 1000 * 60 * 20  # 20 min
+
+    # clear folder contents before processing
+    os.system("rm -rf chunks/*")
+    os.system("rm -rf transcriptions/*")
+
+    total_chunks = len_in_ms // chunk_length_ms + 1
+
+    start_time = time.time()
+
+    for i in range(0, len_in_ms, chunk_length_ms):
+        print(f"Processing chunk {i} to {i + chunk_length_ms}")
+        chunk = audio_segment[i : i + chunk_length_ms]
+        chunk.export(f"chunks/chunk_{i}.mp3", format="mp3")
+
+        # get transcription
+        print("Transcribing/translating chunk...")
+        audio_file = open(f"chunks/chunk_{i}.mp3", "rb")
+        # This worked! :)
+        transcription = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file,
+            language="fr",
+        )
+
+        end_time = time.time()
+
+        print(f"Translation took {end_time - start_time:.2f} seconds")
+
+        # write to file in /transcriptions
+        with open("transcriptions/transcription_" + vid_uuid + ".txt", "a") as f:
+            f.write(transcription.text)
+        current_chunk = i // chunk_length_ms + 1
+        percentage_completion = (current_chunk / total_chunks) * 100
+        print(f"Completed {percentage_completion:.2f}%")
--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-from pytubefix import YouTube
-from pytubefix.cli import on_progress
-from openai import OpenAI
-import os
 from dotenv import load_dotenv
-from pydub import AudioSegment
-import time
-from helpers import dl_yt_audio
-
-load_dotenv()
-
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-client = OpenAI(api_key=OPENAI_API_KEY)
+from helpers import dl_yt_audio, transcribe_or_translate_audio
 
 
 yt_vid_link = "https://www.youtube.com/watch?v=bWfq8Re30Fg"
@@ -21,47 +8,11 @@ vid_uuid, len_in_ms, vid_title = dl_yt_audio(link=yt_vid_link, target_path="down
 
 file_path = "downloads/" + vid_title + ".mp3"
 
-audio_segment = AudioSegment.from_file(file_path, "mp4")
-
-chunk_length_ms = 1000 * 60 * 20  # 20 min
-
-# clear folder contents before processing
-os.system("rm -rf chunks/*")
-os.system("rm -rf transcriptions/*")
-
-total_chunks = len_in_ms // chunk_length_ms + 1
-
-start_time = time.time()
-
-for i in range(0, len_in_ms, chunk_length_ms):
-    print(f"Processing chunk {i} to {i + chunk_length_ms}")
-    chunk = audio_segment[i : i + chunk_length_ms]
-    chunk.export(f"chunks/chunk_{i}.mp3", format="mp3")
-
-    # get transcription
-    print("Transcribing chunk...")
-    print("Translating chunk...")
-    audio_file = open(f"chunks/chunk_{i}.mp3", "rb")
-    # This worked! :)
-    transcription = client.audio.transcriptions.create(
-        model="whisper-1",
-        file=audio_file,
-        language="fr",
-    )
-
-    end_time = time.time()
-
-    print(f"Translation took {end_time - start_time:.2f} seconds")
-
-    # write to file in /transcriptions
-    with open("transcriptions/transcription_" + vid_uuid + ".txt", "a") as f:
-        f.write(transcription.text)
-    current_chunk = i // chunk_length_ms + 1
-    percentage_completion = (current_chunk / total_chunks) * 100
-    print(f"Completed {percentage_completion:.2f}%")
+transcribe_or_translate_audio(
+    file_path=file_path, len_in_ms=len_in_ms, vid_uuid=vid_uuid
+)
 
 # translate to other languages
-
 lang_choices = ["es", "fr", "de", "it", "ja"]
 
 # Display the language choices to the user
