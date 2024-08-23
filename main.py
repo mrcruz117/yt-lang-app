d603462ce37177adf2fe834abf7759022ed29d20
mrcruz117 <mrcruz1989@gmail.com>
2024-08-22 21:36:59 -0400

    organized commands

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from helpers import (
-    # dl_yt_audio,
-    transcribe_or_translate_audio,
-    lang_select,
-    generate_corrected_transcript,
-    text_to_speech,
-    export_audio,
-)
+from helpers import export_audio, lang_select
 from command_funcs.yt import get_yt_data, dl_yt_audio
 from command_funcs.transcribe_audio import transcribe_audio
 from command_funcs.translate_convert import translate_convert_text
@@ -35,25 +28,7 @@ def transcribe():
 
 @app.command(help="Translates text to different languages and CEFR level.")
 def translate():
-    generate_corrected_transcript()
-
-
-# @app.command(help="Download, transcribe, and translate a YouTube video.")
-# def dl():
-#     vid_uuid, len_in_ms, vid_title = dl_yt_audio(target_path="downloads")
-#     lang_selection = lang_select()
-
-#     file_path = "downloads/" + vid_title + ".mp3"
-
-#     transcribe_or_translate_audio(
-#         file_path=file_path, len_in_ms=len_in_ms, vid_uuid=vid_uuid, lang=lang_selection
-#     )
-
-
-@app.command(help="Convert a transcript to a different CEFR level.")
-def convert():
-    path = "transcriptions/jlSsxyWxlaM_es.txt"
-    generate_corrected_transcript(path)
+    translate_convert_text()
 
 
 @app.command(help="Text to speech.")
@@ -66,12 +41,10 @@ def export():
     export_audio()
 
 
-@app.command(help="dev testing command")
-def test():
-    # dl_yt_audio()
-    # transcribe_audio()
-    # translate_convert_text()
-    text_to_speech()
+# @app.command(help="dev testing command")
+# def test():
+
+#     text_to_speech()
 
 
 if __name__ == "__main__":
